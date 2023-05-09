da4d2181e7210e675bb807b5a756abe5efb09c7b
mayank_jelly <mayank.sharma@akalinfosys.com>
2023-05-09 10:37:29 +0530

    change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: 'dashboard'
-  },
-  { path: 'dashboard', component: DashboardComponent },
+  // {
+  //   path: '',
+  //   pathMatch: 'full',
+  //   redirectTo: 'dashboard'
+  // },
+  // { path: 'dashboard', component: DashboardComponent },
 ];
 
 @NgModule({
